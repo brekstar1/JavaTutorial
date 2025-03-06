aa8addf97e265625e3c1cb8a5632dd2b4f0fdebe
brekstar1 <98savuandrei@gmail.com>
2025-03-06 19:18:13 +0200

    Am invatat despre constante.

---
+++ src/test/java/constant/ConstantConcept.java
@@ -0,0 +1,19 @@
+package constant;
+
+import org.testng.annotations.Test;
+
+public class ConstantConcept {
+
+    //Constanta = conceptul prin care definim o proprietate care nu isi mai modifica valoarea initiala
+
+    public final String nume = "Andrei";
+
+    @Test
+    public void testMethod(){
+
+        System.out.println(nume);
+
+        //nume = "Alex";
+        System.out.println(nume);
+    }
+}
